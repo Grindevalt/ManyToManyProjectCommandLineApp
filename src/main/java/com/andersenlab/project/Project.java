3649f33dbefdcab10a3c627bdd4dde9f731857df
Grindevalt <v.badilovschi@andersenlab.com>
2017-03-28 15:42:41 +0300

    first commit

--- src/main/java/com/andersenlab/project/Project.java
+++ src/main/java/com/andersenlab/project/Project.java
@@ -2,10 +2,11 @@ package com.andersenlab.project;
 
 import com.andersenlab.dao.CourseDao;
 import com.andersenlab.dao.StudentDao;
-import com.andersenlab.entities.Course;
-import com.andersenlab.entities.Student;
 import com.andersenlab.util.Factory;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.SQLException;
 
 
@@ -17,39 +18,15 @@ import java.sql.SQLException;
  */
 public class Project {
     @SuppressWarnings("unchecked")
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, IOException {
         Factory factory = Factory.getInstance();
         StudentDao studentDao = factory.getStudentDao();
         CourseDao courseDao = factory.getCourseDao();
-
-        Student valera = new Student("Valera");
-        Student katya = new Student("Katya");
-        Student vasya = new Student("Vasya");
-        Student gosha = new Student("Gosha");
-
-
-        Course history = new Course("History");
-        Course geography = new Course("Geography");
-        Course chemistry = new Course("Chemistry");
-        Course physics = new Course("Physics");
-
-
-        valera.getCourses().add(history);
-        valera.getCourses().add(geography);
-        valera.getCourses().add(physics);
-        studentDao.addStudent(valera);
-
-        katya.getCourses().add(chemistry);
-        katya.getCourses().add(geography);
-        studentDao.addStudent(katya);
-
-        vasya.getCourses().add(physics);
-        vasya.getCourses().add(history);
-        studentDao.addStudent(vasya);
-
-        gosha.getCourses().add(chemistry);
-        gosha.getCourses().add(physics);
-        studentDao.addStudent(gosha);
-
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            Menu.startMenu(studentDao, courseDao, reader);
+        }finally {
+            reader.close();
+        }
     }
 }
